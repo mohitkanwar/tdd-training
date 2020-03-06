c1bdc2ae3ba20d9e9b1cd4cf426cc12e485b0b60
Mohit Kanwar <m.m.kanwar@gmail.com>
2020-03-06 17:53:33 +0530

    fixing the bug

--- PrimeNumberTestr/src/main/java/com/xebia/training/prime/PrimeNumberValidator.java
+++ PrimeNumberTestr/src/main/java/com/xebia/training/prime/PrimeNumberValidator.java
@@ -11,7 +11,7 @@ public class PrimeNumberValidator {
 	
 	public boolean isPrime(int number) {
 		
-		for(int i=2;i<number/2;i++) {
+		for(int i=2;i<=number/2;i++) {
 			if(number%i==0) {
 				return false;
 			}
